a3dd8d71cd3d4b5afe6569778e4b3ec382aca021
Do Nhat <dmnhat.pt@gmail.com>
2025-04-03 11:47:39 +0700

    Adding JobsView.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,4 +1,5 @@
 import HomeView from '@/views/HomeView.vue'
+import JobsView from '@/views/JobsView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/jobs',
+      name: 'jobs',
+      component: JobsView,
     },
   ],
 })
