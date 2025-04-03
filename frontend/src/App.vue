7d595d9f5ef4877966ec4b7aa6a6fd4badf2c1db
Do Nhat <dmnhat.pt@gmail.com>
2025-04-03 10:09:39 +0700

    Adding old fe.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,1 +1,11 @@
-<template>Hello world</template>
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue'
+</script>
+
+<template>
+  <Navbar />
+  <Hero />
+  <HomeCards />
+</template>
